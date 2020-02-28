c7740327befaa94235ac993123f7d6205b835d95
Anastasia <48135406+linguistandmaths@users.noreply.github.com>
2020-02-29 01:05:33 +0700

    I add load dataset

--- set_TE/set_extractor.py
+++ set_TE/set_extractor.py
@@ -1,5 +1,10 @@
+import os
+import t
+
+
 def _extract_set(text, file_with_set):
-    ''' загружаем примеры set из файла резултат храним списком в set_exaples'''
+    """загружаем примеры set из файла резултат храним списком в set_exaples
+    """
     with open((str(file_with_set), encoding='utf-8')) as f:
       set_examples = set(f.readlines())
     ''' перебираем токены из текста, список с кортежами храним в set '''
@@ -21,4 +26,30 @@ def _extract_set(text, file_with_set):
                 else:
                     set.append((token, 'O'))
     return set
-       
+
+
+# считываем файлы с тестовой выборкой, токены соединяются по точке
+def load_dataset(path_to_data_dir):
+    data_files = sorted(os.listdir(path_to_data_dir))
+    test_sentences = []
+    for data_file in tqdm(data_files, desc='Loading data'):
+        with open(os.path.join(path_to_data_dir, data_file), 'r') as data_f:
+            reader = csv.DictReader(data_f)
+            sentence = []
+            for row in reader:
+                if row['token'] == '.':
+                    sentence.append(row['token'])
+                    test_sentences.append(sentence)
+                    sentence = []
+                    continue
+                else:
+                    sentence.append(row['token'])
+    return test_sentences
+
+
+# process test dataset by rules
+if __name__ == '__main__':
+    path_to_data_dir = r'/Users/anast/PycharmProjects/AutoTimeEx/test'
+    sentences = load_dataset(path_to_data_dir)
+    print('Loaded {} sentences'.format(str(len(sentences))))
+

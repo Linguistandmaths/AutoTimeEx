cda05cb3daf68b77d871c7cbcee692e34f6a2c19
Anastasia <48135406+linguistandmaths@users.noreply.github.com>
2020-02-28 23:21:43 +0700

    I am doing code for duration extract

--- duration_TE/duration_extract.py
+++ duration_TE/duration_extract.py
@@ -1,1 +1,14 @@
-#
+# -*- coding: utf-8 -*-
+
+def _extract_duration(text):
+    with open('duration.txt', encoding='uts-8') as f:
+        endings = f.readlines()[:13]
+        starts = f.readlines()[15:]
+    for token in text:
+
+    duration = []
+
+    return duration
+duration.append((token, 'B-DURATION'))
+duration.append((token, 'I-DURATION'))
+duration.append((token, 'O'))

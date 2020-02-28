896d70965f5de9a8546af0472d6ee3a499b9aff6
Anastasia <48135406+linguistandmaths@users.noreply.github.com>
2020-02-29 01:12:23 +0700

    I try to process set type on dataset

--- set_TE/set_extractor.py
+++ set_TE/set_extractor.py
@@ -1,31 +1,33 @@
 import os
-import t
+import csv
+
+from tqdm import tqdm
 
 
 def _extract_set(text, file_with_set):
     """загружаем примеры set из файла резултат храним списком в set_exaples
     """
-    with open((str(file_with_set), encoding='utf-8')) as f:
-      set_examples = set(f.readlines())
+    with open(str(file_with_set), encoding='utf-8') as f:
+        set_examples = set(f.readlines())
     ''' перебираем токены из текста, список с кортежами храним в set '''
-    set = []
+    res_set = []
     for token in text:
-    # проверяем для примеров из одного слова
+        # проверяем для примеров из одного слова
         if token in set_examples:
-            set.append((token, 'B-SET'))
+            res_set.append((token, 'B-SET'))
             continue 
         else:
-               #перебираем список из примеров из файла
+            # перебираем список из примеров из файла
             for set_ex in set_examples:
                 if set_ex.startswith(token):
-                    set.append((token, 'B-SET')) 
+                    res_set.append((token, 'B-SET'))
                     break
                 elif token in set_ex:
-                    set.append((token, 'I-SET')) 
+                    res_set.append((token, 'I-SET'))
                     break
                 else:
-                    set.append((token, 'O'))
-    return set
+                    res_set.append((token, 'O'))
+    return res_set
 
 
 # считываем файлы с тестовой выборкой, токены соединяются по точке
@@ -52,4 +54,10 @@ if __name__ == '__main__':
     path_to_data_dir = r'/Users/anast/PycharmProjects/AutoTimeEx/test'
     sentences = load_dataset(path_to_data_dir)
     print('Loaded {} sentences'.format(str(len(sentences))))
+    with open('set_result.csv', 'w', encoding='utf-8') as result:
+        writer = csv.writer(result)
+        for sentence in sentences:
+            res = _extract_set(sentence,'list_set_type.txt')
+            writer.writerows(res)
+
 
